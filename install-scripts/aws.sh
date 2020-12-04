cf1e672c3fbc9fd9fbf7c10cd5412a6effd51e33
Ricardo Bermúdez Bermúdez <ricardob@localhost.localdomain>
2020-12-03 19:45:50 -0600

    Fix scripts to mongo, maven and add aws install

---
+++ install-scripts/aws.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pip3 install awscli --upgrade --user
+aws --version
+
+
--- install-scripts/mongo.sh
+++ install-scripts/mongo.sh
@@ -3,7 +3,7 @@
 #---- MONGO DB ----
 #Source: https://docs.mongodb.com/manual/tutorial/install-mongodb-on-suse/
 rpm --import https://www.mongo.org/static/pgp/server-4.4.asc
-zypper addrepo --gpgcheck "https://repo.mongodb.org/zypper/suse/12/mongodb-org/4.4/x86_64/" mongodb
+zypper addrepo --gpgcheck "https://repo.mongodb.org/zypper/suse/15/mongodb-org/4.4/x86_64/" mongodb
 # For earlier versions use the follow repo
 # zypper addrepo --no-gpgcheck https://repo.mongodb.org/zypper/suse/12/mongodb-org/3.4/x86_64/ mongodb
 zypper -n install mongodb-org
