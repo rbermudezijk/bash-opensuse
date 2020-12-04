e6fa9e43d3350ba70d5fb29c2330d8182f1c2693
Ricardo Bermúdez Bermúdez <ricardob@localhost.localdomain>
2020-12-03 19:38:55 -0600

    Fix maven install script

--- install-scripts/maven.sh
+++ install-scripts/maven.sh
@@ -5,7 +5,7 @@
 # Maven installation tutorial https://medium.com/@sarathkumarsivan/install-apache-maven-on-suse-linux-enterprise-server-4e411086f52e
 wget https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz 
 tar -xvzf apache-maven-3.6.3-bin.tar.gz -C /opt
-sudo ln -s /opt/apache-maven-3.6.3 /usr/share/maven
+ln -s /opt/apache-maven-3.6.3 /usr/share/maven
 export M2_HOME=/usr/share/maven 
 export PATH=${M2_HOME}/bin:${PATH}
 source ~/.bashrc
